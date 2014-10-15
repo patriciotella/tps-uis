48a1a9f7404f9799ac32ef723fac9e5979d0747f
juliancalvento@gmail.com <juliancalvento@gmail.com@9193ac76-ecb4-0246-70fc-3511fe80cdb5>
2014-10-15 22:35:08 +0000

    Config

--- HtmlViewTP2/carmen-sandiego/app/controllers/Application.java
+++ HtmlViewTP2/carmen-sandiego/app/controllers/Application.java
@@ -2,13 +2,22 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
+import villano.Villano;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+//	iniciarJuego que devuelve un caso + los posibles villanos para pedir la orden de arresto
+	
+//	pistaDelLugar que espera un lugar y un caso y devuelve la pista
 
+//	emitirOrdenPara que espera un villano y un caso y devuelve ok o nok
+
+//	viajar que espera un destino y un caso y devuelve el nuevo estado del caso
+	//(lista de fallidos - viajes posibles - lugares del pais - lugares recorridos)
+	
+	public void ejemplo(){
+		Villano uno;
+	}
 }
+
--- HtmlViewTP2/carmen-sandiego/build.sbt
+++ HtmlViewTP2/carmen-sandiego/build.sbt
@@ -2,6 +2,10 @@ name := "carmen-sandiego"
 
 version := "1.0-SNAPSHOT"
 
+autoScalaLibrary := false
+
+scalaVersion := "2.11.2"
+
 libraryDependencies ++= Seq(
   javaJdbc,
   javaEbean,
