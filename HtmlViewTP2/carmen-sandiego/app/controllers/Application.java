f6d31baf57b8084f36862db85d3ef9df49290a40
juliancalvento@gmail.com <juliancalvento@gmail.com@9193ac76-ecb4-0246-70fc-3511fe80cdb5>
2014-10-21 14:13:43 +0000

    Primeros bindings. Haaaarmosos

--- HtmlViewTP2/carmen-sandiego/app/controllers/Application.java
+++ HtmlViewTP2/carmen-sandiego/app/controllers/Application.java
@@ -1,11 +1,25 @@
 package controllers;
 
+import carmenSanDiegoUIs.JuegoDefaultFactory;
+import carmenSanDiegoUIs.ResolverMisterioModelApp;
+import carmenSanDiegoUIs.XXXFactory;
+import pais.Pais;
 import play.*;
 import play.mvc.*;
 import views.html.*;
 import villano.Villano;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import play.libs.Json;
+
 public class Application extends Controller {
+	
+	private static ResolverMisterioModelApp xxx = new ResolverMisterioModelApp(XXXFactory.xxxDefault().getExpedientes());
 
 //	iniciarJuego que devuelve un caso + los posibles villanos para pedir la orden de arresto
 	
@@ -16,8 +30,54 @@ public class Application extends Controller {
 //	viajar que espera un destino y un caso y devuelve el nuevo estado del caso
 	//(lista de fallidos - viajes posibles - lugares del pais - lugares recorridos)
 	
-	public void ejemplo(){
-		Villano uno;
+    public static Result prueba() {
+		Villano uno = new Villano("", "", null, null);
+		return ok();
 	}
+    
+    public static Result iniciarJuego() throws JsonProcessingException {
+    	ObjectMapper mapper = new ObjectMapper();
+    	mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    	// do various things, perhaps:
+    	xxx.iniciarJuego(JuegoDefaultFactory.getJuegoDefault(XXXFactory.xxxDefault()));
+    	String someJsonString = mapper.writeValueAsString(xxx);
+    	return ok(someJsonString);
+//    	return ok(Json.toJson(xxx));
+    }
+    
+    public static Result pistaDelLugar(String nombreDelLugar) {
+    	String pista = xxx.getPistaDePaisActual(nombreDelLugar);
+    	return ok(Json.toJson(pista));
+    }
+    
+    public static Result emitirOrdenPara(String villanoNombre) {
+    	try{
+    		Villano villano = xxx.getVillano(villanoNombre);
+    		xxx.emitirOrdenDeArresto(villano);
+    		return ok("Orden de arresto emitida correctamente");
+    	} catch (RuntimeException e){
+    		return notFound("La orden de arresto ya fue emitida");
+    	}
+    }
+    
+    public static Result viajar(String nombreDelDestino) {
+    	Pais destino = xxx.getDestino(nombreDelDestino);
+    	xxx.viajarADestino(destino);
+    	return ok(Json.toJson(xxx.getJuegoActual()));
+    }
+    
+    //***********************************************************
+    protected static ObjectNode crearJsonError(String message) {
+    	ObjectNode result = Json.newObject();
+		result.put("error", message);
+		return result;
+	}
+    
+    protected static ObjectNode crearJsonOk() {
+    	ObjectNode result = Json.newObject();
+		result.put("status", "OK");
+		return result;
+	}
+
 }
 
--- HtmlViewTP2/carmen-sandiego/target/scala-2.10/classes/public/javascripts/controllers.js
+++ HtmlViewTP2/carmen-sandiego/target/scala-2.10/classes/public/javascripts/controllers.js
@@ -1,69 +1,87 @@
 'use strict';
 
-var app = angular.module('carmenSanDiegoApp',[])
+var app = angular.module('carmenSanDiegoApp', [])
 
-app.controller('PaisesCtrl', function ($scope) {
-	
-	$scope.paises = [
-	                 {'nombrePais' : 'Uruguay'},
-	                 {'nombrePais' : 'Chile'},
-	                 {'nombrePais' : 'Marruecos'}
-	];
-	
-	$scope.paisActual ={'nombrePais' : 'Argentina'};
-	
-	$scope.paisesFallidos = [
-	                         {'nombrePais' : 'Lisboa'},
-	                         {'nombrePais' : 'Israel'}
-	];
-	
-	$scope.destinos = [
-	                 {'nombrePais' : 'Egipto'},
-	                 {'nombrePais' : 'Rusia'},
-	                 {'nombrePais' : 'Brasil'}
-	];
+app.controller('Controller', function($scope, $http) {
 
-});
+	$scope.iniciarJuego = function() {
+    	$http.get('/iniciarJuego')
+    		.success(function(data) {
+    			$scope.villanos = data.villanos.villanos;
+    			$scope.paisActual = data.juegoActual.paisActual
+    			$scope.descripcionDelCaso = data.juegoActual.descripcionDelCaso
+    			console.log(data.juegoActual.paisActual);
+    		}
+    	)
+    }
+    
+    $scope.iniciarJuego();
+    
+	$scope.paises = [ {
+		'nombrePais' : 'Uruguay'
+	}, {
+		'nombrePais' : 'Chile'
+	}, {
+		'nombrePais' : 'Marruecos'
+	} ];
 
-app.controller('LugaresCtrl', function ($scope) {
+//	$scope.paisActual = {
+//		'nombrePais' : 'Argentina',
+//		'lugaresDeInteres' : [ {
+//			'nombreLugar' : 'Embajada'
+//		}, {
+//			'nombreLugar' : 'Museo'
+//		}, {
+//			'nombreLugar' : 'Biblioteca'
+//		} ]
+//	};
+
+	$scope.paisesFallidos = [ {
+		'nombrePais' : 'Lisboa'
+	}, {
+		'nombrePais' : 'Israel'
+	} ];
+
+	$scope.destinos = [ {
+		'nombrePais' : 'Egipto'
+	}, {
+		'nombrePais' : 'Rusia'
+	}, {
+		'nombrePais' : 'Brasil'
+	} ];
+
+//	$scope.villanos = [
+//			{
+//				'nombre' : 'Carmen Sandiego',
+//				'sexo' : 'Femenino',
+//				'señas_particulares' : [ 'Pelo rojo', 'maneja un convertible',
+//						'posee un collar de rubies',
+//						'su comida favorita son los tacos mexicanos' ],
+//				'hobbies' : 'Juega Tenis'
+//			},
+//			{
+//				'nombre' : 'Carlos Monte',
+//				'sexo' : 'Masculino',
+//				'señas_particulares' : [ 'Pelo Negro', 'maneja una moto',
+//						'posee un collar de rubies',
+//						'su comida favorita son los tacos mexicanos' ],
+//				'hobbies' : 'Juega Futbol'
+//			},
+//			{
+//				'nombre' : 'Daniel Miller',
+//				'sexo' : 'Masculino',
+//				'señas_particulares' : [ 'Pelo Negro', 'maneja una moto',
+//						'posee un collar de rubies',
+//						'su comida favorita son los tacos mexicanos' ],
+//				'hobbies' : 'Juega Futbol'
+//			},
+//			{
+//				'nombre' : 'Maria Romualda de los Valles',
+//				'sexo' : 'Femenino',
+//				'señas_particulares' : [ 'Pelo Negro', 'maneja una moto',
+//						'posee un collar de rubies',
+//						'su comida favorita son los tacos mexicanos' ],
+//				'hobbies' : 'Juega Futbol'
+//			} ]
 	
-	$scope.lugares = [
-	                  {'nombreLugar' : 'Embajada'},
-	                  {'nombreLugar' : 'Museo'},
-	                  {'nombreLugar' : 'Biblioteca'}
-	];
-});
-
-
-app.controller('VillanosCtrl', function ($scope) {
-	
-	$scope.villanos = [
-	                   {'nombre': 'Carmen Sandiego',
-	       	'sexo': 'Femenino',
-    	   'señas_particulares': ['Pelo rojo',
-    	   						  'maneja un convertible',
-    	   						  'posee un collar de rubies',
-    	   						  'su comida favorita son los tacos mexicanos'],
-    	   'hobbies': 'Juega Tenis'},
-    	   {'nombre': 'Carlos Monte',
-   	       	'sexo': 'Masculino',
-       	   'señas_particulares': ['Pelo Negro',
-       	   						  'maneja una moto',
-       	   						  'posee un collar de rubies',
-       	   						  'su comida favorita son los tacos mexicanos'],
-       	   'hobbies': 'Juega Futbol'},
-       	{'nombre': 'Daniel Miller',
-      	       	'sexo': 'Masculino',
-          	   'señas_particulares': ['Pelo Negro',
-          	   						  'maneja una moto',
-          	   						  'posee un collar de rubies',
-          	   						  'su comida favorita son los tacos mexicanos'],
-          	   'hobbies': 'Juega Futbol'},
-          	 {'nombre': 'Maria Romualda de los Valles',
-          	       	'sexo': 'Femenino',
-              	   'señas_particulares': ['Pelo Negro',
-              	   						  'maneja una moto',
-              	   						  'posee un collar de rubies',
-              	   						  'su comida favorita son los tacos mexicanos'],
-              	   'hobbies': 'Juega Futbol'}] 
 });
