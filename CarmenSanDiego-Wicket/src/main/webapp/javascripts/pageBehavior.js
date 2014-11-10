f8df2c2c5b7decd38f7a3acb6447a92f82e6bdfa
patriciotella@gmail.com <patriciotella@gmail.com@9193ac76-ecb4-0246-70fc-3511fe80cdb5>
2014-11-10 23:44:22 +0000

    En el model, esta el HomPageModApp que contiene los expedientes y el mapamundi.
    Despues usamos los editores de pais y villano como formularios para edicion y creacion.

--- CarmenSanDiego-Wicket/src/main/webapp/javascripts/pageBehavior.js
+++ CarmenSanDiego-Wicket/src/main/webapp/javascripts/pageBehavior.js
@@ -1,23 +1,3 @@
-function casoTab() {
-	$('#myTab a:first').off('click');
-	$("#casoBody").show();
-}
-
-function jugarTab() {
-	$("#casoBody").hide();
-}
-
-function mostrarNombreDelLugar(id) {
-	$('.titulo')[0].innerHTML = 'Est&aacute;s visitando: '
-			+ id;
-	$('.titulo')[0].id = id;
-}
-
-function mostrarNombreVillano(idVillano) {
-	document.getElementById('modalVillano').innerHTML = 'Est&aacute;s inspeccionando a: '
-			+ idVillano
-}
-
 function expedientesTab() {
 	$("#mapamundiTab")[0].setAttribute("class","");
 	$("#expedientesTab")[0].setAttribute("class","active")
