019db2ac459a24cd050d2e75249c759e20f039b4
patriciotella@gmail.com <patriciotella@gmail.com@9193ac76-ecb4-0246-70fc-3511fe80cdb5>
2014-12-08 00:46:59 +0000

    cambios locos

--- trunk/android/gen/com/carmensandiego/R.java
+++ trunk/android/gen/com/carmensandiego/R.java
@@ -1648,7 +1648,7 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f090043;
+        public static final int action_settings=0x7f09004d;
         public static final int activity_chooser_view_content=0x7f090021;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
@@ -1660,6 +1660,7 @@ containing a value of this type.
         public static final int disableHome=0x7f09000d;
         public static final int dropdown=0x7f09001a;
         public static final int edit_query=0x7f090033;
+        public static final int emitir_orden_btn=0x7f090042;
         public static final int end=0x7f09001c;
         public static final int expand_activities_button=0x7f090022;
         public static final int expanded_menu=0x7f090028;
@@ -1668,13 +1669,21 @@ containing a value of this type.
         public static final int icon=0x7f090026;
         public static final int ifRoom=0x7f090016;
         public static final int image=0x7f090023;
+        public static final int iniciar_btn=0x7f090045;
         public static final int listMode=0x7f09000a;
         public static final int list_item=0x7f090025;
+        public static final int lugar_1_btn=0x7f090046;
+        public static final int lugar_2_btn=0x7f090047;
+        public static final int lugar_3_btn=0x7f090048;
         public static final int middle=0x7f09001d;
         public static final int never=0x7f090017;
         public static final int none=0x7f09000f;
         public static final int normal=0x7f09000b;
-        public static final int pistas_btn=0x7f090042;
+        public static final int orden_btn=0x7f090049;
+        public static final int pais_1_btn=0x7f09004a;
+        public static final int pais_2_btn=0x7f09004b;
+        public static final int pais_3_btn=0x7f09004c;
+        public static final int pistas_btn=0x7f090044;
         public static final int progress_circular=0x7f090007;
         public static final int progress_horizontal=0x7f090008;
         public static final int radio=0x7f09002b;
@@ -1692,7 +1701,6 @@ containing a value of this type.
         public static final int showCustom=0x7f090010;
         public static final int showHome=0x7f090011;
         public static final int showTitle=0x7f090012;
-        public static final int spinner1=0x7f090040;
         public static final int split_action_bar=0x7f090004;
         public static final int submit_area=0x7f09003c;
         public static final int tabMode=0x7f09000c;
@@ -1700,7 +1708,9 @@ containing a value of this type.
         public static final int ubicacion_actual=0x7f09003f;
         public static final int up=0x7f090001;
         public static final int useLogo=0x7f090013;
-        public static final int viajar_btn=0x7f090041;
+        public static final int viajar_btn=0x7f090043;
+        public static final int villano_acusado=0x7f090040;
+        public static final int villanos_a_acusar=0x7f090041;
         public static final int withText=0x7f090018;
         public static final int wrap_content=0x7f090009;
     }
@@ -1754,7 +1764,10 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
         public static final int activity_main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int iniciar_juego=0x7f030018;
+        public static final int pistas=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int viajar=0x7f03001b;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1818,11 +1831,17 @@ containing a value of this type.
         public static final int abc_toolbar_collapse_description=0x7f0a0004;
         public static final int action_settings=0x7f0a0015;
         public static final int app_name=0x7f0a0010;
+        public static final int conexion_button=0x7f0a001d;
         public static final int emitirTitle=0x7f0a0014;
+        public static final int emitir_button=0x7f0a0019;
+        public static final int iniciar_button=0x7f0a001a;
         public static final int ordenEmitida=0x7f0a0013;
         public static final int ordenTitle=0x7f0a0012;
+        public static final int orden_button=0x7f0a0018;
+        public static final int pistasTitle=0x7f0a001b;
         public static final int pistas_button=0x7f0a0017;
         public static final int ubicacion=0x7f0a0011;
+        public static final int viajarTitle=0x7f0a001c;
         public static final int viajar_button=0x7f0a0016;
     }
     public static final class style {
